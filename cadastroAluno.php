fa04ac5a6c735a3df8b395a61b75f262851bbe4a
Thuan Matheus <thuansilva14@gmail.com>
2018-04-26 17:37:31 -0300

    front- cadastroAluno, cadastroBibliotcaria,cadastroProfessor, home e index

--- cadastroAluno.php
+++ cadastroAluno.php
@@ -2,55 +2,120 @@
 <html lang="pt-br">
 <head>
 	<meta charset ="UTF-8"/>
-	<title> Nome Sistema</title>
+	<link rel="icon" href="../Banco/img/favicon.ico" >
+	<title> Cadastrar Aluno</title>
+	<link href="../Banco/css/bootstrap.min.css " rel="stylesheet" >
+	<link href="../Banco/css/cadastrarAluno.css " rel="stylesheet" >
 </head>
 <body>
-	<center>
-	<a class="dropdown-item" href="home.php"> Voltar</a>
-	<br>
-	<br>
-	<div class ="barra-1">
-		CADASTRO: ALUNOS
-		
-	</div>
-	<div class ="barra-2">
+
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
+				<div class="container ">
+				  <a class="navbar-brand h1" href="home.php"> SIGB </a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite" 
+					data-target="#navbarSupportedContent">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSite">
+					  <ul class="navbar-nav ml-auto ">
+						<li class="nav-item dropdown px-2">
+						  <a class="nav-link dropdown-toggle " href="#" id="navbarSite" role="button" 
+						  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastro</a>
+							  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item " href="/Banco/cadastroProf.php">Professor(a)</a>
+								<a class="dropdown-item" href="/Banco/cadastroAluno.php"> Aluno(a)</a>
+								<a class="dropdown-item" href="#"> Livro</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="/Banco/cadastroBibli.php"> Bibliotecario(a)</a>
+							  </div>
+						</li>
+						<li class="nav-item dropdown px-2">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarSite" role="button" 
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pesquisar</a>
+								<div class="dropdown-menu" >
+									<a class="dropdown-item" href="#"> Professor</a>
+									<a class="dropdown-item" href="#"> Aluno</a>
+								  <a class="dropdown-item" href="#"> Livro</a>
+								</div>
+						</li>
+						<ul class="navbar-nav px-2">
+							<li class="nav-item ">
+							   <a class="nav-link" href="#"> Empréstimo</a>
+							</li>
+						</ul>
+						<ul class="navbar-nav px-2">
+							<li class="nav-item ">
+							   <a class="nav-link" href="#"> Devolução</a>
+							</li>
+						</ul>
+					  </ul>
+					   <ul class="navbar-nav ml-auto px-md-4">
+						 <li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">Usuário </a>
+							<div class="dropdown-menu">
+							  <a class="dropdown-item" href="#"> Perfil </a>
+							  <a class="dropdown-item" href="index.php"> Sair</a>
+							</div>
+						 </li>
+					   </ul>
+					</div>
+				</div>
+		</nav>
+
+
+	<div class="container my-3 px-lg-3 " id="divAluno">
+		<fieldset>
+		<legend><h2>Aluno</h2></legend>
 		<form method="POST" action="/Banco/conexao/conexaoCadastroAluno.php">
-			<div class="fundo-formulario">
-				<table id="formulario-cadastro" width="625" border="0">
-					<tr>
-						<td width ="350">Nome</td>
-						<td ><input type="text" size="100" name="nome" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Turma</td>
-						<td ><input type="text" size="100" name="turma" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Responsavel</td>
-						<td ><input type="text" size="100" name="responsavel" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Serie</td>
-						<td ><input type="text" size="100" name="serie" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Matricula</td>
-						<td ><input type="text" size="100" name="matricula" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td><input type="submit" id="salvar" value="Salvar">
-					</tr>
-					<tr>
-						<td><input type="reset" value="Cancelar">
-					</tr>
-				</table>
+			<div class="form-row ">
+			  <div class="form-group col-md-12" >
+				<label >Nome Completo</label>
+				<input type="text" class="form-control" name="nome"required autofocus>
+			  </div>
 			</div>
-		</form>
-	</div>
 	
-	<div class ="barra-3">
+			<div class="form-row  ">
+					<div class="form-group col-md-12 mx-auto" >
+					<label >Responsável</label>
+					<input type="text" class="form-control" name="responsavel" required autofocus>
+					</div>
+				</div>
+				<div class="form-row ">
+					<div class="form-group col-md-3">
+						<label >Turma</label>
+						<input type="text" class="form-control" name="turma" required autofocus>
+					</div>
+				<div class="form-group col-md-4">
+						<label >Série</label>
+						<input type="text" class="form-control" name="serie" required autofocus>
+				</div>
+				<div class="form-group col-md-5">
+						<label>Matrícula</label>
+						<input type="text" class="form-control" name="matricula" required autofocus>
+				</div>
+			</div>
+			<center>
 		
-	</div>
-	</center>
+					 <button type="reset" class="btn btn-danger btn-lg mx-3">Cancelar</button>
+					 <button type="submit" class="btn btn-primary btn-lg mx-3 "> Salvar </button>
+				 
+       </center>
+			 <hr>
+	
+	</form>
+	
+</fieldset>
+</div>
+
+
+
+ <!-- Bootstrap JavaScript
+    ================================================== -->
+    <!-- -->
+    <script src="./js/slim.min.js" ></script>
+    <script src="./js/popper.min.js" ></script>
+    <script src="./js/bootstrap.min.js" ></script>
+		
+	
 </body>
 </html>
--- cadastroBibli.php
+++ cadastroBibli.php
@@ -1,68 +1,132 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+<head>
 	<meta charset ="UTF-8"/>
-	<title> Nome Sistema</title>
+	<link rel="icon" href="../Banco/img/favicon.ico" >
+	<title> Cadastrar Biliotecario(a)</title>
+	<link href="../Banco/css/bootstrap.min.css " rel="stylesheet" >
+	<link href="../Banco/css/cadastrarAluno.css " rel="stylesheet" >
+
 </head>
 <body>
-	<center>
-	<a class="dropdown-item" href="home.php"> Voltar</a>
-	<br>
-	<br>
-	<div class ="barra-1">
-		CADASTRO: BIBLIOTECÁRIA
-		
-	</div>
-	<div class ="barra-2">
-		<form method="POST" action="/Banco/conexao/conexaoCadastroBibli.php">
-			<div class="fundo-formulario">
-				<table id="formulario-cadastro" width="625" border="0">
-					<tr>
-						<td width ="350">Nome</td>
-						<td ><input type="text" size="100" name="nome" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Email</td>
-						<td ><input type="Email" size="100" name="email" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Telefone</td>
-						<td ><input type="text" size="100" name="telefone" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Rua</td>
-						<td ><input type="text" size="100" name="rua" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Numero</td>
-						<td ><input type="text" size="100" name="numero" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Bairro</td>
-						<td ><input type="text" size="100" name="bairro" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Login</td>
-						<td ><input type="text" size="100" name="login" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td width ="350">Senha</td>
-						<td ><input type="password" size="100" name="senha" maxlength="100"/></td>
-					</tr>
-					<tr>
-						<td><input type="submit" id="salvar" value="Salvar">
-					</tr>
-					<tr>
-						<td><input type="reset" value="Cancelar">
-					</tr>
-				</table>
-			</div>
-		</form>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
+				<div class="container ">
+				  <a class="navbar-brand h1" href="home.php"> SIGB </a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite" 
+					data-target="#navbarSupportedContent">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSite">
+					  <ul class="navbar-nav ml-auto ">
+						<li class="nav-item dropdown px-2">
+						  <a class="nav-link dropdown-toggle " href="#" id="navbarSite" role="button" 
+						  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastro</a>
+							  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item " href="/Banco/cadastroProf.php">Professor(a)</a>
+								<a class="dropdown-item" href="/Banco/cadastroAluno.php"> Aluno(a)</a>
+								<a class="dropdown-item" href="#"> Livro</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="/Banco/cadastroBibli.php"> Bibliotecario(a)</a>
+							  </div>
+						</li>
+						<li class="nav-item dropdown px-2">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarSite" role="button" 
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pesquisar</a>
+								<div class="dropdown-menu" >
+									<a class="dropdown-item" href="#"> Professor</a>
+									<a class="dropdown-item" href="#"> Aluno</a>
+								  <a class="dropdown-item" href="#"> Livro</a>
+								</div>
+						</li>
+						<ul class="navbar-nav px-2">
+							<li class="nav-item ">
+							   <a class="nav-link" href="#"> Empréstimo</a>
+							</li>
+						</ul>
+						<ul class="navbar-nav px-2">
+							<li class="nav-item ">
+							   <a class="nav-link" href="#"> Devolução</a>
+							</li>
+						</ul>
+					  </ul>
+					   <ul class="navbar-nav ml-auto px-md-4">
+						 <li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">Usuário </a>
+							<div class="dropdown-menu">
+							  <a class="dropdown-item" href="#"> Perfil </a>
+							  <a class="dropdown-item" href="index.php"> Sair</a>
+							</div>
+						 </li>
+					   </ul>
+					</div>
+				</div>
+	</nav>
 	
-	<div class ="barra-3">
+	
+	
+	<div class="container my-3 px-lg-3" id="divAluno">
+	<fieldset>
+		<legend><h2>Bibliotecario(a)</h2></legend>
+	
+		<form  class method="POST" action="/Banco/conexao/conexaoCadastroBibli.php">
+		<div class="form-row ">
+			<div class="form-group col-md-6">
+				<label >Nome Completo</label>
+				<input type="text" class="form-control" name="nome" required autofocus>
+			</div>
+			<div class="form-group col-md-4 ">
+				<label >Email</label>
+				<input type="email" class="form-control"  placeholder="exemple@exemple.com" name="email" required autofocus>
+			</div>
+			<div class="form-group col-md-2">
+				<label for="inputtelefone" >Telefone</label>
+				<input type="tel" class= "form-control" id="inputtelefone" placeholder="(xx)xxxxx-xxxx" name="telefone"> 
+			</div>
+		</div>
+
+		<div class="form-row ">
+			<div class="form-group col-md-5">
+				<label >Rua</label>
+				<input type="text" class="form-control"placeholder="Betel"  name="rua" required autofocus>
+			</div>
+			<div class="form-group col-md-5">
+				<label >Bairro</label>
+				<input type="text" class="form-control" placeholder="Santa Cruz" name="bairro">
+			</div>
+			<div class="form-group col-md-2">
+				<label >Número</label>
+				<input type="tel" class="form-control" placeholder="xx"  name="numero" required autofocus>
+			</div>
+		</div>
 		
-	</div>
-	</center>
+		<div class="form-row " >
+			<div class="form-group col-md-4 ">
+				<label for="inputlogin">Login</label>
+				<input type="text" class="form-control" id="inputlogin" placeholder="exemple"  name="login" required autofocus>
+			</div>
+			
+			<div class="form-label-group col-md-4 ">
+			<label >Senha</label>
+            <input type="password" name ="senha"  placeholder="password" class="form-control"  required autofocus>     
+        </div>
+		</div>
+	
+	
+		<center>
+			<button type="reset" class="btn btn-danger btn-lg mx-3">Cancelar</button>
+			<button type="submit" class="btn btn-primary btn-lg mx-3 "> Salvar </button>
+		<hr>
+		  </center>
+	</fieldset>
+</div>
+				
+</form>
+ <!-- Bootstrap JavaScript
+    ================================================== -->
+    <!-- -->
+    <script src="./js/slim.min.js" ></script>
+    <script src="./js/popper.min.js" ></script>
+    <script src="./js/bootstrap.min.js" ></script>
 </body>
 </html>
